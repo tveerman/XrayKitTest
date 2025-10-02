046dec6e6d2a7d24a9cfc382b25f31aedda2dde7
Thomas Veerman <thomas.veerman@wanwire.net>
2025-10-02 17:45:47 +0200

    Add convenience wrapper

--- Package.swift
+++ Package.swift
@@ -7,11 +7,15 @@ let package = Package(
   platforms: [.iOS(.v16), .macOS(.v14), .macCatalyst(.v16), .tvOS(.v17), .visionOS(.v1)],
   products: [
     .library(
-        name: "XrayCoreKit",
-        targets: ["Binary"]
+        name: "XrayKit",
+        targets: ["XrayKit"]
     )
   ],
   targets: [
+    .target(
+        name: "XrayKit",
+        dependencies: ["Binary"]
+    ),
     .binaryTarget(
         name: "Binary",
 	url: "https://github.com/tveerman/XrayKitTest/releases/download/1.0.2/XrayCoreKit.xcframework.zip",
