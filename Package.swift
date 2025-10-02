3f60a6d1cc4740abfc6a6ef3d06b341a9757da0d
Thomas Veerman <thomas.veerman@wanwire.net>
2025-10-02 14:09:11 +0200

    rename

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
   platforms: [.iOS(.v16), .macOS(.v14), .macCatalyst(.v16), .tvOS(.v17), .visionOS(.v1)],
   products: [
     .library(
-        name: "XrayKit",
-        targets: ["LibXray"]
+        name: "XCKit",
+        targets: ["XCBinary"]
     )
   ],
   targets: [
     .binaryTarget(
-        name: "LibXray",
-	url: "https://github.com/tveerman/XrayKitTest/releases/download/1.0.0/Libxraygo.xcframework.zip",
+        name: "XCBinary",
+	url: "https://github.com/tveerman/XrayKitTest/releases/download/1.0.1/Libxraygo.xcframework.zip",
 	checksum: "b5794831d9bcd0ab8539e9ba39e7e5c367b074ecaa6beb7c618878a385020bac"
     )
   ]
