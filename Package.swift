a321c524afca0ec605330ce096d29b44c8a6a449
Thomas Veerman <thomas.veerman@wanwire.net>
2025-10-02 14:41:25 +0200

    1.0.2

--- Package.swift
+++ Package.swift
@@ -7,15 +7,15 @@ let package = Package(
   platforms: [.iOS(.v16), .macOS(.v14), .macCatalyst(.v16), .tvOS(.v17), .visionOS(.v1)],
   products: [
     .library(
-        name: "XCKit",
-        targets: ["XCBinary"]
+        name: "XrayCoreKit",
+        targets: ["Binary"]
     )
   ],
   targets: [
     .binaryTarget(
-        name: "XCBinary",
-	url: "https://github.com/tveerman/XrayKitTest/releases/download/1.0.1/Libxraygo.xcframework.zip",
-	checksum: "b5794831d9bcd0ab8539e9ba39e7e5c367b074ecaa6beb7c618878a385020bac"
+        name: "Binary",
+	url: "https://github.com/tveerman/XrayKitTest/releases/download/1.0.2/XrayCoreKit.xcframework.zip",
+	checksum: "d5a39ad78cbb8c730153e351059e5954d65ded2a65886abb76924980b60fc789"
     )
   ]
 )
